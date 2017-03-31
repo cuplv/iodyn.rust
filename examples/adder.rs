c3f968850c4204539914bc49763b7ccd925c6a3a
Kyle Headley <kyle.headley@colorado.edu>
2017-03-30 19:16:42 -0600

    create visualization for adder example; need to change central accumulator

--- examples/adder.rs
+++ examples/adder.rs
@@ -7,26 +7,25 @@ extern crate rand;
 
 extern crate adapton_lab;
 
-// use std::fs::OpenOptions;
-// use std::io::Write;
-// use time::Duration;
 use rand::{Rand,Rng,StdRng,SeedableRng};
 use eval::actions::*;
 use eval::interface::{IFaceSeq,IFaceNew,IFaceArchive};
 #[allow(unused)] use pmfp_collections::IRaz;
+#[allow(unused)] use pmfp_collections::inc_archive_stack::AStack as IAStack;
 #[allow(unused)] use eval::eval_nraz::EvalNRaz;
 #[allow(unused)] use eval::eval_iraz::EvalIRaz;
 #[allow(unused)] use eval::eval_vec::EvalVec;
 #[allow(unused)] use eval::eval_iastack::EvalIAStack;
+#[allow(unused)] use eval::accum_lists::*;
 use eval::test_seq::{TestMResult,EditComputeSequence};
 use pmfp_collections::inc_gauged_raz::{AtTail};
-use pmfp_collections::inc_archive_stack::AStack as IAStack;
 use adapton::engine::*;
 use adapton::engine::manage::*;
 use adapton_lab::labviz::*;
-use std::io::prelude::*;
 use std::io::BufWriter;
 use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 /// Input Lang
 ///
@@ -136,16 +135,18 @@ fn main2() {
       -n, --namesize=[namesize] 'initial tree nodes between each art'
       -e, --edits=[edits]       'edits per batch'
       -c, --changes=[changes]   'number of incremental changes'
-      -o, --outfile=[outfile]   'name for output files (of different extensions)' ")
+      -o, --outfile=[outfile]   'name for output files (of different extensions)'
+      --trace                   'produce an output trace of the incremental run' ")
     .get_matches();
   let dataseed = value_t!(args, "data_seed", usize).unwrap_or(0);
   let editseed = value_t!(args, "edit_seed", usize).unwrap_or(0);
-	let start_size = value_t!(args, "start", usize).unwrap_or(10_000);
-	let unitgauge = value_t!(args, "unitsize", usize).unwrap_or(100);
+	let start_size = value_t!(args, "start", usize).unwrap_or(1_000_000);
+	let unitgauge = value_t!(args, "unitsize", usize).unwrap_or(1_000);
 	let namegauge = value_t!(args, "namesize", usize).unwrap_or(1);
 	let edits = value_t!(args, "edits", usize).unwrap_or(1);
 	let changes = value_t!(args, "changes", usize).unwrap_or(30);
   let outfile = args.value_of("outfile");
+  let do_trace = args.is_present("trace");
 
 	let coord = StdRng::from_seed(&[dataseed]);
 
@@ -198,7 +199,7 @@ fn main2() {
 		num.archive((l,n))
 	}
 	let name_to_tree = name_of_string(String::from("to_tree"));
-  let mut test = EditComputeSequence{
+  let mut test_inc = EditComputeSequence{
     init: IncrementalInit {
       size: start_size,
       unitgauge: unitgauge,
@@ -234,37 +235,146 @@ fn main2() {
 		),
     changes: changes,
   };
+  let mut test_non_inc = EditComputeSequence{
+    init: IncrementalInit {
+      size: start_size,
+      unitgauge: unitgauge,
+      namegauge: namegauge,
+      coord: coord.clone(),
+    },
+    edit: BatchInsert(edits),
+    // The type here determins the type of the accumulator
+    // TODO: Move this parameter elsewhere
+    comp: Compute2::<_,_,_,EvalVec<Token,StdRng>,_>::new(
+    	MFolder::new(
+    		name_of_string(String::from("tokenize")),
+				(IFaceNew::new(),None),
+				tokenize_step,
+				tokenize_meta,
+				|a|{
+					let ts = tokenize_final(a);
+					ns(name_to_tree.clone(),||{IncrementalFrom{
+						data: ts, // This determings the accumulator type
+			      unitgauge: unitgauge,
+			      namegauge: namegauge,
+			      coord: coord.clone(),
+					}.create(&mut StdRng::new().unwrap()).1})
+				},
+			),
+    	MFolder::new(
+    		name_of_string(String::from("parse")),
+    		IFaceNew::new(),
+    		parse_step,
+    		parse_meta,
+    		|a|{a},
+    	)
+		),
+    changes: changes,
+  };
 
   init_dcg(); assert!(engine_is_dcg());
-  // for visual debugging
-  reflect::dcg_reflect_begin();
+
 
   // run experiments
+
   let mut rng = StdRng::from_seed(&[editseed]);
-  let result: TestMResult<
-  	EvalIRaz<Lang,StdRng>, // in type
-  	IAStack<u32,u32>,  // out type
-  > = test.test(&mut rng);
-  // let result: TestMResult<
-  // 	EvalVec<Lang,StdRng>,
-  // 	Vec<u32>,
-  // > = test.test(&mut rng);
+
+  let result_non_inc: TestMResult<
+  	EvalVec<Lang,StdRng>,
+  	Vec<u32>,
+  > = test_non_inc.test(&mut rng);
 
   // for visual debugging
-  let traces = reflect::dcg_reflect_end();
-  let f = File::create("trace.html").unwrap();
-  let mut writer = BufWriter::new(f);
-  writeln!(writer, "{}", style_string()).unwrap();
-  writeln!(writer, "<div class=\"label\">Editor trace({}):</div>", traces.len()).unwrap();
-  writeln!(writer, "<div class=\"traces\">").unwrap();
-  for tr in traces {
-  	div_of_trace(&tr).write_html(&mut writer);
+  if do_trace {reflect::dcg_reflect_begin()}
+
+  let result_inc: TestMResult<
+  	EvalIRaz<Lang,StdRng>, // in type
+  	RcVecList<u32>,  // out type
+  > = test_inc.test(&mut rng);
+
+  if do_trace {
+	  let traces = reflect::dcg_reflect_end();
+
+	  // output trace
+	  let f = File::create("trace.html").unwrap();
+	  let mut writer = BufWriter::new(f);
+	  writeln!(writer, "{}", style_string()).unwrap();
+	  writeln!(writer, "<div class=\"label\">Editor trace({}):</div>", traces.len()).unwrap();
+	  writeln!(writer, "<div class=\"traces\">").unwrap();
+	  for tr in traces {
+	  	div_of_trace(&tr).write_html(&mut writer);
+	  }
+	}
+
+  // post-process results
+
+
+  let edit_non_inc = result_non_inc.edits.iter().map(|d|d.num_nanoseconds().unwrap()).collect::<Vec<_>>();
+  let edit_inc = result_inc.edits.iter().map(|d|d.num_nanoseconds().unwrap()).collect::<Vec<_>>();
+  let comp_non_inc = result_non_inc.computes.iter().map(|d|(d[0].num_nanoseconds().unwrap(),d[1].num_nanoseconds().unwrap())).collect::<Vec<_>>();
+  let comp_inc = result_inc.computes.iter().map(|d|(d[0].num_nanoseconds().unwrap(),d[1].num_nanoseconds().unwrap())).collect::<Vec<_>>();
+  
+  println!("computes_non_inc(tokenize,parse): {:?}", comp_non_inc);
+  println!("computes_inc(tokenize,parse): {:?}", comp_inc);
+
+
+  // generate data file
+
+
+  let filename = if let Some(f) = outfile {f} else {"out"};
+
+  let mut dat: Box<Write> =
+    Box::new(
+      OpenOptions::new()
+      .create(true)
+      .write(true)
+      .truncate(true)
+      .open(filename.to_owned()+".dat")
+      .unwrap()
+    )
+  ;
+
+  let (mut en,mut tn,mut pn,mut ei,mut ti,mut pi) = (0f64,0f64,0f64,0f64,0f64,0f64);
+  writeln!(dat,"'{}'\t'{}'\t'{}'\t'{}'","Changes","Edit Time","Tokenize Time","Parse Time").unwrap();
+  for i in 0..changes {
+    en += edit_non_inc[i] as f64 / 1_000_000.0;
+    tn += comp_non_inc[i].0 as f64 / 1_000_000.0;
+    pn += comp_non_inc[i].1 as f64 / 1_000_000.0;
+    writeln!(dat,"{}\t{}\t{}\t{}",i,en,tn,pn).unwrap();    
+  }
+  writeln!(dat,"").unwrap();
+  writeln!(dat,"").unwrap();
+  for i in 0..changes {
+    ei += edit_inc[i] as f64 / 1_000_000.0;
+    ti += comp_inc[i].0 as f64 / 1_000_000.0;
+    pi += comp_inc[i].1 as f64 / 1_000_000.0;
+    writeln!(dat,"{}\t{}\t{}\t{}",i,ei,ti,pi).unwrap();    
   }
 
-  println!("inc times(ns) (tokenize,parse): {:?}", result.computes.iter().map(|c|{
-  	(c[0].num_nanoseconds().unwrap(),c[1].num_nanoseconds().unwrap())
-  }).collect::<Vec<_>>());
+  let mut plotscript =
+    OpenOptions::new()
+    .create(true)
+    .write(true)
+    .truncate(true)
+    .open(filename.to_owned()+".plotscript")
+    .unwrap()
+  ;
 
-  // TODO: chart results
+  // generate plot script
+
+  writeln!(plotscript,"set terminal pdf").unwrap();
+  writeln!(plotscript,"set output '{}'", filename.to_owned()+".pdf").unwrap();
+  write!(plotscript,"set title \"{}", "Accumulating time to insert element(s) and parse\\n").unwrap();
+  writeln!(plotscript,"(s)ize: {}, (u)nit-gauge: {}, (n)ame-gauge: {}, (e)dit-batch: {}\"", start_size,unitgauge,namegauge,edits).unwrap();
+  writeln!(plotscript,"set xlabel '{}'", "(c)hanges").unwrap();
+  writeln!(plotscript,"set ylabel '{}'","Time(ms)").unwrap();
+  writeln!(plotscript,"set key left top box").unwrap();
+  writeln!(plotscript,"plot \\").unwrap();
+  writeln!(plotscript,"'{}' i 0 u 1:($3+$4) t '{}' with lines,\\",filename.to_owned()+".dat","Non-incremental Parse Time").unwrap();
+  writeln!(plotscript,"'{}' i 1 u 1:($3+$4) t '{}' with lines,\\",filename.to_owned()+".dat","Incremental Parse Time").unwrap();
+
+  //generate plot
+
+  ::std::process::Command::new("gnuplot").arg(filename.to_owned()+".plotscript").output().unwrap();
 
 }
