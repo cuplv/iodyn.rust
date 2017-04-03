26738a0dec2433daf899607d99a855eef1113dba
Matthew Hammer <matthew.hammer@colorado.edu>
2017-04-03 15:53:07 -0600

    add trie to lib

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ pub mod inc_archive_stack;
 pub mod inc_level_tree;
 pub mod inc_tree_cursor;
 pub mod inc_gauged_raz;
+pub mod inc_gauged_trie;
 
 /// Persistent Raz - original design, simple but works
 pub type PRaz<E> = persist_raz::Raz<E>;
