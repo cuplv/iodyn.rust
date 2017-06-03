eccbcff3068e67055794f88747d1ed5315fa28ab
Matthew Hammer <matthew.hammer@colorado.edu>
2017-06-03 14:55:35 -0600

    new four-constructor Trie representation

--- src/trie2.rs
+++ src/trie2.rs
@@ -17,7 +17,7 @@ pub mod hammer_level_tree {
     use std::fmt::Debug;
     use std::hash::{Hash};
     use adapton::macros::*;
-    use adapton::engine::{Name,Art,cell,force};
+    use adapton::engine::{Name,Art,cell,force,thunk,ArtIdChoice};
     use raz::RazTree;
     use raz_meta::Count;
 
@@ -59,13 +59,13 @@ pub mod hammer_level_tree {
             LevTree{rec:Rec::Bin(BinCons{level:lev,recl:Box::new(l.rec),recr:Box::new(r.rec)})} 
         }
         pub fn name(lev:u32, n:Name, r:Self) -> Self {
-            LevTree{rec:Rec::Name(NameCons{level:lev,name:n.clone(), rec:Box::new(r.rec)}) }
+            LevTree{rec:Rec::Name(NameCons{level:lev,name:n, rec:Box::new(r.rec)}) }
         }
         fn art(a:Art<Rec<X>>) -> Self {
             LevTree{rec:Rec::Art(a)}
         }
-        pub fn fold_monoid<B:Clone>
-            (t:LevTree<X>, z:X, b:B, bin:fn(B,X,X)->X) -> X {
+        pub fn fold_monoid<B:'static+Clone+PartialEq+Eq+Debug+Hash>
+            (t:LevTree<X>, z:X, b:B, bin:fn(B,X,X)->X, art:fn(Art<X>,X)->X) -> X {
                 fn m_leaf<B:Clone,X>(m:(B,fn(B,X,X)->X,X), elms:Vec<X>) -> X { 
                     let mut x = m.2;
                     for elm in elms { x=m.1(m.0.clone(), x, elm) };
@@ -78,10 +78,15 @@ pub mod hammer_level_tree {
                                         (B,fn(B,X,X)->X,X),X>
                     (t,
                      (b.clone(),bin,z.clone()), m_leaf,
-                     (b,bin,z), m_bin)
+                     (b,bin,z), m_bin, 
+                     art)
             }
 
-        pub fn fold_up<L:Clone,B:Clone,N:Clone,R>
+        pub fn fold_up
+            <L:'static+Clone+PartialEq+Eq+Debug+Hash,
+             B:'static+Clone+PartialEq+Eq+Debug+Hash,
+             N:'static+Clone+PartialEq+Eq+Debug+Hash,
+             R:'static+Clone+PartialEq+Eq+Debug+Hash>
             (t:LevTree<X>, 
              l:L, leaf:fn(L,Vec<X>)->R, 
              b:B, bin:fn(B,u32,R,R)->R,
@@ -89,7 +94,10 @@ pub mod hammer_level_tree {
                 Self::fold_up_rec(t.rec, l, leaf, b, bin, n, name)
             }
         
-        fn fold_up_rec<L:Clone,B:Clone,N:Clone,R>
+        fn fold_up_rec<L:'static+Clone+PartialEq+Eq+Debug+Hash,
+                       B:'static+Clone+PartialEq+Eq+Debug+Hash,
+                       N:'static+Clone+PartialEq+Eq+Debug+Hash,
+                       R:'static+Clone+PartialEq+Eq+Debug+Hash>
             (t:Rec<X>, 
              l:L, leaf:fn(L,Vec<X>)->R, 
              b:B, bin:fn(B,u32,R,R)->R,
@@ -109,31 +117,46 @@ pub mod hammer_level_tree {
                 }
             }
 
-        pub fn fold_up_namebin<L:Clone,B:Clone,R>
+        pub fn fold_up_namebin
+            <L:'static+Clone+PartialEq+Eq+Debug+Hash,
+             B:'static+Clone+PartialEq+Eq+Debug+Hash,
+             R:'static+Clone+PartialEq+Eq+Debug+Hash>
             (t:LevTree<X>, 
              l:L, leaf:fn(L,Vec<X>)->R, 
-             b:B, namebin:fn(B,u32,Option<Name>,R,R)->R) -> R {
-                Self::fold_up_namebin_rec(t.rec, l, leaf, b, None, namebin)
+             b:B, namebin:fn(B,u32,Option<Name>,R,R)->R, 
+             art:fn(Art<R>,R)->R) -> R {
+                Self::fold_up_namebin_rec(t.rec, l, leaf, b, None, namebin, art)
             }
         
-        fn fold_up_namebin_rec<L:Clone,B:Clone,R>
-            (t:Rec<X>, 
+        fn fold_up_namebin_rec
+            <L:'static+Clone+PartialEq+Eq+Debug+Hash,
+             B:'static+Clone+PartialEq+Eq+Debug+Hash,
+             R:'static+Clone+PartialEq+Eq+Debug+Hash>             
+            (t:Rec<X>,
              l:L, leaf:fn(L,Vec<X>)->R,
-             b:B, n:Option<Name>, namebin:fn(B,u32,Option<Name>,R,R)->R) -> R {
-                 match t {
-                    Rec::Art(a) => Self::fold_up_namebin_rec(get!(a), l, leaf, b, n, namebin),
-                     Rec::Leaf(leafcons) => leaf(l, leafcons.elms),
-                     Rec::Bin(bincons)   => {
-                         let r1 = Self::fold_up_namebin_rec(*bincons.recl, l.clone(), leaf, b.clone(), None, namebin);
-                         let r2 = Self::fold_up_namebin_rec(*bincons.recr, l.clone(), leaf, b.clone(), None, namebin);
-                         namebin(b, bincons.level, n, r1, r2)
-                     }
-                    Rec::Name(namecons) => {
-                        Self::fold_up_namebin_rec(*namecons.rec, l, leaf, b, Some(namecons.name.clone()), namebin)
+             b:B, n:Option<Name>,
+             namebin:fn(B,u32,Option<Name>,R,R)->R,
+             art:fn(Art<R>,R)->R) -> R {
+                match t {
+                    Rec::Art(a) => Self::fold_up_namebin_rec(get!(a), l, leaf, b, n, namebin, art),
+                    Rec::Leaf(leafcons) => leaf(l, leafcons.elms),
+                    Rec::Bin(bincons)   => {
+                        let r1 = Self::fold_up_namebin_rec(*bincons.recl, l.clone(), leaf, b.clone(), None, namebin, art);
+                        let r2 = Self::fold_up_namebin_rec(*bincons.recr, l.clone(), leaf, b.clone(), None, namebin, art);
+                        namebin(b, bincons.level, n, r1, r2)
                     }
-                 }
+                    Rec::Name(namecons) => {
+                        let (a,x) = eager!( 
+                            namecons.name.clone() =>> Self::fold_up_namebin_rec,
+                            t:*namecons.rec, 
+                            l:l, leaf:leaf,
+                            b:b, n:Some(namecons.name), namebin:namebin, art:art 
+                        );
+                        art(a,x)
+                    }
+                }
             }
-
+        
         pub fn from_raz_tree(t:RazTree<X,Count>) -> LevTree<X> {
             fn at_leaf<X:'static+Clone+PartialEq+Eq+Debug+Hash>
                 (v:&Vec<X>) -> LevTree<X> {
@@ -197,25 +220,32 @@ pub struct TrieMeta {
 }
 
 #[derive(PartialEq,Eq,Clone,Debug,Hash)]
-enum TrieRec<K:'static+Hash+PartialEq+Eq+Clone+Debug,
+pub enum TrieRec<K:'static+Hash+PartialEq+Eq+Clone+Debug,
              V:'static+Hash+PartialEq+Eq+Clone+Debug>
 {
     Empty,
     Leaf(TrieLeaf<K,V>),
     Bin(TrieBin<K,V>),
+    Name(TrieName<K,V>),
+    Art(Art<TrieRec<K,V>>),
 }
 #[derive(PartialEq,Eq,Clone,Debug,Hash)]
-struct TrieLeaf<K:'static+Hash+PartialEq+Eq+Clone+Debug,
-                V:'static+Hash+PartialEq+Eq+Clone+Debug> {
+pub struct TrieLeaf<K:'static+Hash+PartialEq+Eq+Clone+Debug,
+                    V:'static+Hash+PartialEq+Eq+Clone+Debug> {
     kvs:Rc<Vec<(K,HashVal,V)>>,
 }
 #[derive(Hash,PartialEq,Eq,Clone,Debug)]
-struct TrieBin<K:'static+Hash+PartialEq+Eq+Clone+Debug,
-               V:'static+Hash+PartialEq+Eq+Clone+Debug> {
+pub struct TrieBin<K:'static+Hash+PartialEq+Eq+Clone+Debug,
+                   V:'static+Hash+PartialEq+Eq+Clone+Debug> {
     bits:   Bits,
+    left:   Box<TrieRec<K,V>>,
+    right:  Box<TrieRec<K,V>>,
+}
+#[derive(Hash,PartialEq,Eq,Clone,Debug)]
+pub struct TrieName<K:'static+Hash+PartialEq+Eq+Clone+Debug,
+                    V:'static+Hash+PartialEq+Eq+Clone+Debug> {
     name:   Name,
-    left:   Art<TrieRec<K,V>>,
-    right:  Art<TrieRec<K,V>>,
+    rec:    Box<TrieRec<K,V>>,
 }
 
 impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
@@ -231,6 +261,8 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
 
     fn find_rec (t: &Trie<K,V>, r:&TrieRec<K,V>, h:HashVal, h_rest:HashVal, k:&K) -> Option<V> {
         match r {
+            &TrieRec::Art(ref a) => Self::find_rec(t, &get!(a), h, h_rest, k),
+            &TrieRec::Name(ref n) => Self::find_rec(t, &*n.rec, h, h_rest, k),
             &TrieRec::Empty => None,
             &TrieRec::Leaf(ref l) => {
                 let mut ans = None;
@@ -243,9 +275,9 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
             },
             &TrieRec::Bin(ref b) => {
                 if h_rest.0 & 1 == 0 {
-                    Self::find_rec(t, &get!(b.left), h, HashVal(h_rest.0 >> 1), k)
+                    Self::find_rec(t, &*b.left, h, HashVal(h_rest.0 >> 1), k)
                 } else { 
-                    Self::find_rec(t, &get!(b.right), h, HashVal(h_rest.0 >> 1), k)
+                    Self::find_rec(t, &*b.right, h, HashVal(h_rest.0 >> 1), k)
                 }
             }
         }
@@ -307,10 +339,10 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
              rec:TrieRec::Leaf(TrieLeaf{kvs:Rc::new(vec)})}
     }
 
-    pub fn join (lt: Self, rt: Self, n:Name) -> Self {
+    pub fn join (n:Option<Name>, lt: Self, rt: Self) -> Self {
         //assert_eq!(lt.gauge, rt.gauge); // ??? -- Or take the min? Or the max? Or the average?
         let gauge = if lt.meta.gauge > rt.meta.gauge { lt.meta.gauge } else { rt.meta.gauge };
-        Trie{rec:Self::join_rec(TrieMeta{gauge:gauge}, lt.rec, rt.rec, Bits{len:0, bits:0}, n),..lt}
+        Trie{rec:Self::join_rec(TrieMeta{gauge:gauge}, n, lt.rec, rt.rec, Bits{len:0, bits:0}),..lt}
     }
 
     fn split_bits (bits:&Bits) -> (Bits, Bits) {
@@ -328,6 +360,8 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
 
     fn is_wf_rec (t:&TrieRec<K,V>, bits:Bits) -> bool {
         match *t {
+            TrieRec::Art(ref a) => Self::is_wf_rec(&get!(a), bits),
+            TrieRec::Name(ref n) => Self::is_wf_rec(&*n.rec, bits),
             TrieRec::Empty => true,
             TrieRec::Leaf(ref leaf) => {
                 // Check that all of the hash values match the given bit pattern of bits.
@@ -342,8 +376,8 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
             // Check bit patterns match, and that recursive trees are well-formed.
             TrieRec::Bin(ref b) => { 
                 let (b0, b1) = Self::split_bits(&bits);
-                let lwf = Self::is_wf_rec(& get!(b.left), b0);
-                let rwf = Self::is_wf_rec(& get!(b.right), b1);
+                let lwf = Self::is_wf_rec(&*b.left, b0);
+                let rwf = Self::is_wf_rec(&*b.right, b1);
                 b.bits == bits && lwf && rwf 
             }
         }
@@ -353,6 +387,90 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
         Self::is_wf_rec(&self.rec, Bits{bits:0, len:0})
     }
 
+    fn join_rec (meta:TrieMeta, n:Option<Name>, lt: TrieRec<K,V>, rt: TrieRec<K,V>, bits:Bits) -> TrieRec<K,V> {
+        match n {
+            Some(n) => {
+                let (a,_trie) = eager!(n.clone() =>> Self::join_rec, meta:meta, n:None, lt:lt, rt:rt, bits:bits);
+                TrieRec::Name(TrieName{name:n, rec:Box::new(TrieRec::Art(a))})
+            },
+            None => { match (lt, rt) {
+                (TrieRec::Empty,   TrieRec::Empty)   => TrieRec::Empty,
+                (TrieRec::Empty,   TrieRec::Leaf(r)) => TrieRec::Leaf(r),
+                (TrieRec::Leaf(l), TrieRec::Empty  ) => TrieRec::Leaf(l),
+
+                (TrieRec::Name(n), rt) => { Self::join_rec (meta, Some(n.name), *n.rec, rt, bits) },
+                (lt, TrieRec::Name(n)) => { Self::join_rec (meta, None,         lt, *n.rec, bits) },
+                
+                (TrieRec::Art(a), rt) => { Self::join_rec (meta, n, get!(a), rt, bits) },
+                (lt, TrieRec::Art(a)) => { Self::join_rec (meta, n, lt, get!(a), bits) },
+
+                (TrieRec::Leaf(l), TrieRec::Leaf(r)) => {
+                    if l.kvs.len() == 0 { 
+                        TrieRec::Leaf(r)
+                    } else if r.kvs.len() == 0 {
+                        TrieRec::Leaf(l)
+                    } else if l.kvs.len() + r.kvs.len() < meta.gauge {
+                        // Sub-Case: the leaves, when combined, are smaller than the gauge.
+                        let mut vec = (*l.kvs).clone();
+                        for &(ref k, ref k_hash, ref v) in r.kvs.iter() { 
+                            vec.push((k.clone(),k_hash.clone(),v.clone()));
+                        }
+                        Self::leaf_or_empty(vec)
+                    } else {
+                        // Sub-Case: the leaves are large enough to justify not being combined.
+                        let (e0, e1) = (Vec::new(), Vec::new());
+                        let (l0, l1) = Self::split_vec(l.kvs, bits.len, e0, e1);
+                        let (r0, r1) = Self::split_vec(r.kvs, bits.len, l0, l1);
+                        let t0 = Box::new(TrieRec::Leaf(TrieLeaf{kvs:Rc::new(r0)}));
+                        let t1 = Box::new(TrieRec::Leaf(TrieLeaf{kvs:Rc::new(r1)}));
+                        TrieRec::Bin(TrieBin{left:t0, right:t1, bits:bits})
+                    }
+                },
+                (TrieRec::Empty, TrieRec::Bin(r)) => {
+                    let (b0, b1) = Self::split_bits(&bits);
+                    let o0 = Self::join_rec(meta.clone(), None, TrieRec::Empty, *r.left, b0);
+                    let o1 = Self::join_rec(meta.clone(), None, TrieRec::Empty, *r.right, b1);
+                    TrieRec::Bin(TrieBin{ left:Box::new(o0), right:Box::new(o1), bits:bits })
+                },
+                (TrieRec::Leaf(l), TrieRec::Bin(r)) => {
+                    let (e0, e1) = (Vec::new(), Vec::new());
+                    let (l0, l1) = Self::split_vec(l.kvs, bits.len, e0, e1);
+                    let (b0, b1) = Self::split_bits(&bits);
+                    let o0 = Self::join_rec(meta.clone(), None, Self::leaf_or_empty(l0), *r.left, b0);
+                    let o1 = Self::join_rec(meta.clone(), None, Self::leaf_or_empty(l1), *r.right, b1);
+                    TrieRec::Bin(TrieBin{ left:Box::new(o0), right:Box::new(o1), bits:bits })
+                },
+                (TrieRec::Bin(l), TrieRec::Empty) => {
+                    let (b0, b1) = Self::split_bits(&bits);
+                    let o0 = Self::join_rec(meta.clone(), None, *l.left, TrieRec::Empty, b0);
+                    let o1 = Self::join_rec(meta.clone(), None, *l.right, TrieRec::Empty, b1);
+                    TrieRec::Bin(TrieBin{ left:Box::new(o0), right:Box::new(o1), bits:bits })
+                },
+                (TrieRec::Bin(l), TrieRec::Leaf(r)) => {
+                    let (e0, e1) = (Vec::new(), Vec::new());
+                    let (r0, r1) = Self::split_vec(r.kvs, bits.len, e0, e1);
+                    let (b0, b1) = Self::split_bits(&bits);
+                    let o0 = Self::join_rec(meta.clone(), None, *l.left, Self::leaf_or_empty(r0), b0);
+                    let o1 = Self::join_rec(meta.clone(), None, *l.right, Self::leaf_or_empty(r1), b1);
+                    TrieRec::Bin(TrieBin{ left:Box::new(o0), right:Box::new(o1), bits:bits })
+                },
+                (TrieRec::Bin(l), TrieRec::Bin(r)) => {
+                    let test1 = l.bits == bits;
+                    let test2 = l.bits == r.bits;
+                    if !(test1 && test2) {
+                        panic!("\nInternal error: {:?} {:?} -- bits:{:?} l.bits:{:?} r.bits:{:?}!!!\n", 
+                               test1, test2, bits, l.bits, r.bits);
+                    };
+                    let (b0, b1) = Self::split_bits(&bits);
+                    let o0 = Self::join_rec(meta.clone(), None, *l.left,  *r.left, b0);
+                    let o1 = Self::join_rec(meta.clone(), None, *l.right, *r.right, b1);
+                    TrieRec::Bin(TrieBin{ left:Box::new(o0), right:Box::new(o1), bits:bits })
+                }
+            }}
+        }   
+    }
+
+/*
     fn join_rec (meta:TrieMeta, lt: TrieRec<K,V>, rt: TrieRec<K,V>, bits:Bits, n:Name) -> TrieRec<K,V> {
         match (lt, rt) {
             (TrieRec::Empty,   TrieRec::Empty)   => TrieRec::Empty,
@@ -432,7 +550,8 @@ impl<K:'static+Hash+PartialEq+Eq+Clone+Debug,
                 TrieRec::Bin(TrieBin{ left:o0.0, right:o1.0, name:n, bits:bits })
             }
         }
-    }               
+    }
+     */
 }
 
 #[test] pub fn test_join_10_1   () { test_join(10,1) }
@@ -497,12 +616,15 @@ pub fn test_join (size:usize, gauge:usize) {
     fn at_namebin(_:(), _lev:u32,n:Option<Name>,l:Trie<usize,()>,r:Trie<usize,()>) -> Trie<usize,()> {
         assert!(l.is_wf());
         assert!(r.is_wf());
-        ns(n.clone().unwrap(), || Trie::join(l,r,n.unwrap()) )
+        ns(n.clone().unwrap(), || Trie::join(n,l,r) )
+    }
+    fn at_art(_a:Art<Trie<usize,()>>, t:Trie<usize,()>) -> Trie<usize,()> {
+        t 
     }
     let trie = ns( name_of_str("trie_of_lev_tree"),
                    || LevTree::fold_up_namebin( lev_tree,
                                                 (), at_leaf,
-                                                (), at_namebin ) );
+                                                (), at_namebin, at_art ) );
     println!("{:?}\n", trie);
 
     for i in elmv {
